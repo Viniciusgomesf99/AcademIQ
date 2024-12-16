28fabf1fadbe593c2ef75cae1cab0c894563eb41
Viniucius <viniciusgomesf33@gmail.com>
2024-12-15 21:57:14 -0300

    feat: AcademIQ

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { CourseComponent } from './components/course/course.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ConfiguracoesComponent } from './components/configuracoes/configuracoes.component';
 import { MainLayoutComponent } from './components/layout/main-layout.component';
+import { RenderMode } from '@angular/ssr';
 
 export const routes: Routes = [
   {
@@ -11,7 +12,7 @@ export const routes: Routes = [
     component: MainLayoutComponent,
     children: [
       { path: '', component: MeusCursosComponent },
-      { path: 'course/:id', component: CourseComponent },
+      { path: 'course/:id', component: CourseComponent, data: { renderMode: 'default' }},
       { path: 'profile', component: ProfileComponent },
       { path: 'configurations', component: ConfiguracoesComponent },
     ]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
     production: true,
+    pandaApiKey: 'panda-0d04d2a8927cad7ecf3ba9bf6cffd82ade83d284d4a4df0cb733532b8bda2b0c',
+    pandaApiUrl: 'https://api-v2.pandavideo.com.br'
 };
