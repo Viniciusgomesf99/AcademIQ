9c9d461c6356794ba437dd264d52a09aea658e73
Viniucius <viniciusgomesf33@gmail.com>
2024-12-15 22:22:10 -0300

    feat: AcademIQ

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { CourseComponent } from './components/course/course.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ConfiguracoesComponent } from './components/configuracoes/configuracoes.component';
 import { MainLayoutComponent } from './components/layout/main-layout.component';
-import { RenderMode } from '@angular/ssr';
 
 export const routes: Routes = [
   {
@@ -12,7 +11,7 @@ export const routes: Routes = [
     component: MainLayoutComponent,
     children: [
       { path: '', component: MeusCursosComponent },
-      { path: 'course/:id', component: CourseComponent, data: { renderMode: 'default' }},
+      { path: 'course/:id', component: CourseComponent },
       { path: 'profile', component: ProfileComponent },
       { path: 'configurations', component: ConfiguracoesComponent },
     ]
